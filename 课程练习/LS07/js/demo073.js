619c44a24583e8774fe292b534f0a39986c675ee
wan2banwangcen <715586415@qq.com>
2018-03-26 00:05:58 +0800

    V1.7.1 第七个PPT练习（3月26日）

--- 课程练习/LS07/js/demo073.js
+++ 课程练习/LS07/js/demo073.js
@@ -1,25 +1,26 @@
-//值传递
-var a = 1;
-function foo(x) {
-    console.trace("a:",a," x:",x);
-    x = 2;//step 2222
-    console.trace("a:",a," x:",x);//step 3333
+//实参数大于形参数
+function test() {
+    console.log(arguments);//console.log(test.arguments==arguments,arguments);
+    // console.log(arguments.length);
+	// console.log(typeof arguments);
+	// console.log(arguments instanceof Array);
+	// console.log(arguments instanceof Object);
+    console.log(Array.prototype.slice.call(arguments));
+    var s = "";
+    for (var i = 0; i < arguments.length; i++) {
+        s += arguments[i];
+    }
+    return s;
 }
-
-console.trace("a:",a);
-foo(a);// step 1111
-console.trace("a:",a); // step 4444  a仍为1
+test("hello,", "world!");//"hello,world!"
 
 
-//引用传递
-var obj = {x:1};
-function fee(o){
-    console.trace("obj.x :",obj.x," o.x :",o.x);
-    o.x = 3;// step 2222
-    console.trace("obj.x :",obj.x," o.x :",o.x);// step 3333
-}
-
-console.trace("obj.x :",obj.x);
-fee(obj);// step 1111
-console.trace("obj.x :",obj.x);//step 4444  obj.x被改写为3
-
+//实参数小于形参数
+var sum = function(a,b,c){
+    b = b||4;
+    c = c||5;
+    return a+b+c;
+};
+console.log(sum(1,2,3));
+console.log(sum(1,2));
+console.log(sum(1));
