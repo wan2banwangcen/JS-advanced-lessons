3016febac29b59b8b5af734624a7411f65da7683
wan2banwangcen <715586415@qq.com>
2018-03-25 23:26:31 +0800

    V1.6.1 第六个PPT练习（3月25日）

---
+++ 课程练习/LS06/js/demo061.js
@@ -0,0 +1,18 @@
+//逻辑与、或的基本理解
+console.log(2>1&&4<5);
+console.log(true&&(!2));
+console.log(false&&("2" == 2));
+console.log(false&&false);
+
+console.log(2>1||4<5);
+console.log(true||(!2));
+console.log(false||("2" == 2));
+console.log(false||false);
+//true
+//false
+//false
+//false
+//true
+//true
+//true
+//false
